9cc1815f0188608eead1ff2650a280b63e36cd48
Ecaterina Ignatov <stravisnkywellington@gmail.com>
2023-12-10 16:00:50 +0200

    Part 3- Post Note

--- part3/part3_a/index.js
+++ part3/part3_a/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+// Express JSON Parser
+app.use(express.json())
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
 
 let notes = [
     {
@@ -43,6 +52,26 @@ app.get('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id)
   
     response.status(204).end()
+  })
+
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
   })
 
 const PORT = 3001
