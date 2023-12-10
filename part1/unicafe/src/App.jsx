f240140d6246544be85052c2a8ce24aa31e3182f
Ecaterina Ignatov <stravisnkywellington@gmail.com>
2023-12-10 19:35:49 +0200

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,22 +9,31 @@ const App = () => {
 
   const handleUpdateGood = () => setGood(good + 1);
   const handleUpdateBad = () => setBad(bad + 1);
-  const handleUpdateNeutral = () => setNeutral(neutral + 1)
+  const handleUpdateNeutral = () => setNeutral(neutral + 1);
+
+  const total = good + bad + neutral;
+  const average = ((good * 1) + (neutral * 0) - bad) / total;
+  const positive = good / total;
+
+  console.log(total)
 
   return (
     <div>
       <div>
-      <h1>give feedback</h1>
-      <button onClick={handleUpdateGood}>good</button>
-      <button onClick={handleUpdateBad}>bad</button>
-      <button onClick={handleUpdateNeutral}>neutral</button>
+        <h1>give feedback</h1>
+        <button onClick={handleUpdateGood}>good</button>
+        <button onClick={handleUpdateBad}>bad</button>
+        <button onClick={handleUpdateNeutral}>neutral</button>
       </div>
 
       <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>bad {bad}</p>
-      <p>neutral {neutral}</p>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>bad {bad}</p>
+        <p>neutral {neutral}</p>
+        <p>all {total}</p>
+        <p>average {average}</p>
+        <p>positive {positive}%</p>
       </div>
     </div>
   )
