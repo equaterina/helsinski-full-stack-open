3d2cfc22a81a1d281e0394f7eb390daafb81f864
Ecaterina Ignatov <stravisnkywellington@gmail.com>
2023-12-10 15:45:24 +0200

    Part 3 - Add route for single note

--- part3/part3_a/index.js
+++ part3/part3_a/index.js
@@ -28,6 +28,16 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).json({'error': 'Note was not found. Make sure the current note exists.'}).end()
+      }
+  })
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
